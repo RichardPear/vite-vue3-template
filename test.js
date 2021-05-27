a16907f86adf14b1fc8986a9944a388dd315826e
lich45 <lich45@midea.com>
2021-05-27 14:12:24 +0800

    init

---
+++ test.js
@@ -0,0 +1,8 @@
+const dotenv = require('dotenv')
+const result = dotenv.config({path: '.env'})
+
+if (result.error) {
+  throw result.error
+}
+
+console.log(result, result.parsed)
---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import dotenv from 'dotenv';
+import { injectHtml } from 'vite-plugin-html';
+export default ({ command, mode }) => {
+  let parsed = {}
+  let base = '/'
+  let outDir = 'dist'
+  if (command === 'serve') {
+    // 开发模式，默认使用.env配置
+    parsed = dotenv.config().parsed;
+  } else {
+    // build模式，根据mode使用env文件
+    parsed = dotenv.config({ path: `.env.${mode}` }).parsed;
+    base = '/vite/'
+    outDir = 'dist/vite'
+  }
+  return defineConfig({
+    base,
+    plugins: [vue(), injectHtml({ injectData: parsed })],
+    build: {
+      outDir
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://www.toutiao.com/',
+          changeOrigin: true
+        }
+      }
+    }
+  })
+}
